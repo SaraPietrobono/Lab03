6cb3721ceb51757d7d138cac6d544760abadb3c3
SaraPietrobono <sara.pietrobono2006@gmail.com>
2025-10-07 14:14:27 +0200

    inizio lab03

---
+++ auto.py
@@ -0,0 +1,8 @@
+class Automobile:
+    def __init__(self,codice,marca,modello,anno, posti):
+        self.codice = codice
+        self.marca=marca
+        self.modello=modello
+        self.anno=anno
+        self.posti=posti
+
--- autonoleggio.py
+++ autonoleggio.py
@@ -1,15 +1,34 @@
+from auto import Automobile
+import csv
 class Autonoleggio:
-    def __init__(self, nome, responsabile):
+    def __init__(self, nome, responsabile,automobile):
         """Inizializza gli attributi e le strutture dati"""
         # TODO
+        self.nome=nome
+        self.responsabile=responsabile
+        self.automobile=[]
 
     def carica_file_automobili(self, file_path):
         """Carica le auto dal file"""
         # TODO
-
-    def aggiungi_automobile(self, marca, modello, anno, num_posti):
+        try:
+            with open(file_path, 'r',encoding='utf-8') as f:
+                for line in f:
+                    campi=line.strip('').split('')
+                    codice=campi[0]
+                    marca=campi[1]
+                    modello=campi[2]
+                    anno=campi[3]
+                    posti=campi[4]
+                    C=Automobile(codice,marca,modello,anno,posti)
+                    self.automobile.append(C)
+        except FileNotFoundError:
+            return None
+    def aggiungi_automobile(self, marca, modello, anno, num_posti, automobile):
         """Aggiunge un'automobile nell'autonoleggio: aggiunge solo nel sistema e non aggiorna il file"""
         # TODO
+
+
 
     def automobili_ordinate_per_marca(self):
         """Ordina le automobili per marca in ordine alfabetico"""
